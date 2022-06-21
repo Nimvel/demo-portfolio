f5c8d37f2e56ccedfe8864d2fc5fbf437e8e3cf9
Nimvel <rabbit.of.margery@gmail.com>
2022-06-21 14:27:44 +0300

    profile

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -4,12 +4,12 @@ let initialState = {
     profileData: [
         {id: 1, avatarULR: avatar, userName: 'Кристина Кушняревич', jobTitle: 'Trainee/Junior JS React', status: 'В активном поиске работы',
         skills: [
-            {id: 1, skill: 'HTML'},
-            {id: 2, skill: 'CSS'},
-            {id: 3, skill: 'JS'},
-            {id: 4, skill: 'React'},
-            {id: 5, skill: 'Redux'},
-            {id: 6, skill: 'React-Redux'}
+            {id: 1, skill: 'HTML', url: 'https://developer.mozilla.org/ru/docs/Web/HTML'},
+            {id: 2, skill: 'CSS', url: 'https://developer.mozilla.org/ru/docs/Web/CSS/Reference'},
+            {id: 3, skill: 'JS', url: 'https://developer.mozilla.org/ru/docs/Web/JavaScript'},
+            {id: 4, skill: 'React', url: 'https://ru.reactjs.org/docs/getting-started.html'},
+            {id: 5, skill: 'Redux', url: 'https://rajdee.gitbooks.io/redux-in-russian/content/'},
+            {id: 6, skill: 'React-Redux', url: 'https://rajdee.gitbooks.io/redux-in-russian/content/'}
         ],
         aboutMe: [
             {id: 1, feature: 'целеустремленность'},
@@ -18,8 +18,8 @@ let initialState = {
             {id: 4, feature: 'аккуратность'},
         ],
         education: [
-            {id: 1, level: 'ССО', educationalInstitution: 'МГКЭ', speciality: 'ОЭВМ и ОПП'},
-            {id: 2, level: 'неоконченное высшее', educationalInstitution: 'БГТУ', speciality: 'ПОИТ'},
+            {id: 1, level: 'ССО', educationalInstitution: 'МГКЭ', speciality: 'ОЭВМ и ОПП', url: 'http://mgke.minsk.edu.by/'},
+            {id: 2, level: 'неоконченное высшее', educationalInstitution: 'БГТУ', speciality: 'ПОИТ', url: 'https://www.belstu.by/'},
             // {id: 3, level: 'неоконченное высшее', educationalInstitution: 'МГЛУ', speciality: 'переводчик'}
         ],
         languages: [
--- src/сomponents/Profile/Profile.jsx
+++ src/сomponents/Profile/Profile.jsx
@@ -1,26 +1,26 @@
 import style from './Profile.module.css';
 
 const Profile = ({ profileData }) => {
-    let lastSkill = profileData.skills.length;
     let lastFeature = profileData.aboutMe.length;
-    let lastEducation = profileData.education.length;
-    let lastLanguage = profileData.languages.length;
 
-    let skills = profileData.skills.map( s => s.id != lastSkill
-        ? <span className={style.skill} key={s.id}> {s.skill},</span>
-        : <span className={style.skill} key={s.id}> {s.skill}.</span> )
+    let skills = profileData.skills.map(s =>
+            <a key={s.id} href={s.url} target='_blank' >
+                <li className={style.skill} >{s.skill}</li>
+            </a>
 
-    let aboutMe = profileData.aboutMe.map( f => f.id != lastFeature
+    )
+
+    let aboutMe = profileData.aboutMe.map(f => f.id != lastFeature
         ? <span className={style.feature} key={f.id}> {f.feature},</span>
-        : <span className={style.feature} key={f.id}> {f.feature}.</span> )
+        : <span className={style.feature} key={f.id}> {f.feature}.</span>)
 
-    let education = profileData.education.map( e => e.id != lastEducation
-        ? <li className={style.speciality} key={e.id}>{` ${e.level} - ${e.educationalInstitution}: ${e.speciality},`}</li>
-        : <li className={style.speciality} key={e.id}>{` ${e.level} - ${e.educationalInstitution}: ${e.speciality}.`}</li> )
+    let education = profileData.education.map(e =>
+        <a key={e.id} href={e.url} target='_blank' >
+        <li className={style.speciality} key={e.id}>{`${e.level} - ${e.educationalInstitution}: ${e.speciality}`}</li>
+        </a>)
 
-    let languages = profileData.languages.map( l => l.id != lastLanguage
-        ? <li className={style.language} key={l.id}>{` ${l.language} - ${l.level},`}</li>
-        : <li className={style.language} key={l.id}>{` ${l.language} - ${l.level}.`}</li> )
+    let languages = profileData.languages.map(l =>
+        <li className={style.language} key={l.id}>{`${l.language} - ${l.level}`}</li>)
 
     return (
         <div className={style.profilePage}>
@@ -34,9 +34,9 @@ const Profile = ({ profileData }) => {
             </div>
             <div className={style.description}>
                 <div className={style.skills}>Навыки: {skills}</div>
-                <div className={style.about_me}>О себе: {aboutMe}</div>
                 <div className={style.education}>Образование: {education}</div>
                 <div className={style.languages}>Языки: {languages}</div>
+                <div className={style.about_me}>О себе: {aboutMe}</div>
             </div>
         </div>
     )
