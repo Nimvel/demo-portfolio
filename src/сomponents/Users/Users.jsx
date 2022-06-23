3c00224ae62af0b1f1bd26ba278603fa795a568e
Nimvel <rabbit.of.margery@gmail.com>
2022-06-23 14:37:34 +0300

    get users

--- src/сomponents/Users/Users.jsx
+++ src/сomponents/Users/Users.jsx
@@ -5,20 +5,15 @@ import User from './User/User';
 
 import style from './Users.module.css';
 
-// import userImg from '../../assets/icons/comrade.png';
-
 
 const Users = ({ setUsers, usersData, follow, unfollow }) => {
-    if (usersData.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-        .then(response => {
-            setUsers(response.data.items)
-        })
-
-        // setUsers( [
-        // { id: 1, userName: 'Kurapika', userImg: user, isFollowed: false },
-        // { id: 2, userName: 'Lucifer', userImg: user, isFollowed: true },
-        // { id: 3, userName: 'Ledy Maria', userImg: user, isFollowed: false }])
+    let getUsers = () => {
+        if (usersData.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                setUsers(response.data.items)
+            })
+        }
     }
 
     let usersElements = 
@@ -31,6 +26,7 @@ const Users = ({ setUsers, usersData, follow, unfollow }) => {
 
     return (
         <div className={style.usersPage}>
+            <button onClick={getUsers} className={style.get_users}>get users</button>
             <div className={style.users}>
                 {usersElements}
                 
